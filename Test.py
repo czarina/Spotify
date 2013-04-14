8d0918759d4f23dd47bbe87f908afa1a7783f31a
czarina <zou.christina@gmail.com>
2013-04-14 13:47:28 -0400

    v2

--- Test.py
+++ Test.py
@@ -1,5 +1,25 @@
 import sys
 
+def quicksort(target):
+	left = []
+	right = []
+	if(len(target)<=1):
+		return target
+	pivot = int(len(target)/2)
+	pivotVal = target[pivot]
+	target.pop(pivot)
+	for x in range(0, len(target)):
+		if(target[x][2]<pivotVal[2]):
+			left.append(target[x])
+		elif(target[x][2]==pivotVal[2]):
+			if(target[x][3]<pivotVal[3]):
+				right.append(target[x])
+			else:
+				left.append(target[x]) 
+		else:
+			right.append(target[x])
+	return quicksort(left)+[pivotVal]+quicksort(right)
+
 inBuffer = []
 
 n, m = [int(x) for x in sys.stdin.readline().split()]
@@ -10,21 +30,12 @@ for counter in range(0, n):
 	freq = int(newLine[0])
 	name = newLine[1]
 	q = float(freq)/(float(1)/float(counter+1))
-	inBuffer[]
+	inBuffer.append([freq, name, q, counter])
 
-	inBuffer.append([freq, name, q])
+inBuffer = quicksort(inBuffer)
 
-for i in range(0, m):
-	maxIndex = i
-	maxValue = inBuffer[i][2]
-	for j in range(i+1, n):
-		if(inBuffer[j][2]>maxValue):
-			maxIndex = j
-			maxValue = inBuffer[j][2]
-	temp = inBuffer[i]
-	inBuffer[i]=inBuffer[maxIndex]
-	inBuffer[maxIndex]=temp
-	print inBuffer[i][1]
+for x in range(0, m):
+	print(inBuffer[len(inBuffer)-x-1][1])
 
 
 
